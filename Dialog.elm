0c05681bd834b468536529f0bd30f3706a28d6d0
Serge Hänni <serge.haenni@simplificator.com>
2017-08-01 00:39:12 +0200

    fixed menu position in dialog

--- Dialog.elm
+++ Dialog.elm
@@ -21,5 +21,5 @@ renderDialogAt dialog =
   g []
     [ rect [ x "50", y "100", width "400", height "300", Svg.Attributes.style "fill: rgb(21, 3, 183)" ] []
     , text_ [ x "75", y "125", Svg.Attributes.style "fill: rgb(144, 144, 181); font-family: sans-serif" ] [ text dialog.message ]
-    , renderMenu dialog.menu 200 200
+    , renderMenu dialog.menu 150 200
     ]
